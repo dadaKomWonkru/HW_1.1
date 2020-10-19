728c2bd4bbdbe1cb21d2a66296f829421f49ef41
dadaKomWonkru <davit.tsuladze.1@btu.edu.ge>
2020-10-19 13:31:12 +0400

    HW_1

---
+++ database/seeders/bookSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\book;
+use Illuminate\Database\Seeder;
+use function GuzzleHttp\Psr7\str;
+
+class bookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        book::insert([
+            "title" => "Harry Potter and goblet of the fire",
+            "author" => "J.K. Rowling",
+            "pages" =>453,
+            "language"=> "Eng",
+        ]);
+    }
+}
